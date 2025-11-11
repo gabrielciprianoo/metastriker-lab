d20531d804b244d0d856020f8337cb7c8924878c
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-11-11 10:10:47 -0600

    ✨ Create DashboardPage component and redirect Home to Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
 
-
-export default function page() {
-  return (
-    <div className="text-3xl uppercase">page</div>
-  )
+export default function Home() {
+  redirect("/dashboard");
 }
