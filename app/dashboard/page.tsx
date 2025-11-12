1c301393ca84cfb843f766ae44e23876da159b93
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-11-12 16:53:41 -0600

    ✨ Add RegisterMatchModal and integrate openRegisterMatch functionality in DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,12 +14,17 @@ import { useDashboardData } from "./hooks/useDashboardData";
 import { StatCard } from "./components/StatCard";
 import { MatchList } from "./components/MatchList";
 import { MotivationCard } from "./components/MotivationCard";
+import { useAppStore } from "@/stores/useAppStore";
+import { RegisterMatchModal } from "../matches/components/RegisterMatchModal";
 
 export default function DashboardPage() {
   const { analyses, stats, randomPhrase } = useDashboardData();
+  const openRegisterMatch = useAppStore((s) => s.openRegisterMatch);
 
   return (
     <main className="min-h-screen bg-linear-to-b from-[#0b0b0b] via-[#0f1112] to-black text-gray-100 p-6 md:p-10 space-y-20 relative overflow-hidden">
+      <RegisterMatchModal />
+
       <div
         aria-hidden="true"
         className="absolute inset-0 bg-[radial-gradient(circle_at_top,rgba(120,255,180,0.08)_0%,transparent_70%)] blur-3xl -z-10"
@@ -62,7 +67,8 @@ export default function DashboardPage() {
         >
           <Button
             className="relative overflow-hidden bg-linear-to-r cursor-pointer from-emerald-500 to-emerald-400 text-white font-semibold text-lg px-10 py-5 rounded-2xl shadow-md
-    transition-all duration-300 flex items-center justify-center gap-3 hover:shadow-emerald-500/40 hover:scale-[1.03]"
+  transition-all duration-300 flex items-center justify-center gap-3 hover:shadow-emerald-500/40 hover:scale-[1.03]"
+            onClick={openRegisterMatch}
           >
             <span className="absolute inset-0 bg-linear-to-r from-transparent via-white/20 to-transparent opacity-0 hover:opacity-100 blur-sm transition-opacity duration-700 animate-[shine_2.5s_linear_infinite]" />
             <PlayIcon className="h-8 w-8 text-white" />
